b3f490dc78ea128d2df4742d44546c36dc65b4b2
Colton Williams <coltonneilwilliams@gmail.com>
2023-07-16 23:30:54 -0700

    Update layout to use grid and create placeholder budget route

---
+++ src/routes/budgets/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	const newBudget = () => goto('./budgets/create');
+</script>
+
+<h2 class="h2 text-surface-300 opacity-60 col-span-12 justify-self-center self-center">
+	No Budgets
+</h2>
+<button
+	on:click={newBudget}
+	type="button"
+	class="btn-icon btn-icon-l variant-filled place-self-end col-span-12 !bg-primary-500"
+>
+	<i class="fa-solid fa-plus text-2xl" />
+</button>
